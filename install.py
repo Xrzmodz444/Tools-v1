cdd75300878f6cffe7dca0feec593222dec409b4
Xrzmodz444 <148513815+Xrzmodz444@users.noreply.github.com>
2024-02-21 00:44:21 +0700

    Add files via upload

---
+++ install.py
@@ -0,0 +1,17 @@
+import os
+
+# Update dan upgrade paket
+os.system("pkg update && pkg upgrade")
+
+# Instalasi Node.js
+os.system("pkg install nodejs")
+
+# Instalasi Colorama menggunakan pip
+os.system("pip install colorama")
+
+# Instalasi Figlet dan Lolcat
+os.system("pkg install figlet lolcat")
+
+os.system("pkg install mpv")
+
+os.system("npm install")
